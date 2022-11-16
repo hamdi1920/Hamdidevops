7de6ae112d0e52dba4db1c83b6eacdc311f68fdc
hamdi1920 <47883340+hamdi1920@users.noreply.github.com>
2022-11-16 23:25:10 +0100

    Update FactureTests.java

--- src/test/java/tn/esprit/rh/achat/FactureTests.java
+++ src/test/java/tn/esprit/rh/achat/FactureTests.java
@@ -1,55 +1,33 @@
 
-package com.example.demo;
+package tn.esprit.rh.achat;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
-
-import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
-import static org.mockito.Mockito.mock;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.sql.Date;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import org.apache.tomcat.util.http.parser.MediaType;
-import org.aspectj.lang.annotation.Before;
-import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import com.example.demo.controllers.StockRestController;
-import com.example.demo.entities.Produit;
-import com.example.demo.entities.Stock;
-import com.example.demo.repositories.ProduitRepository;
-import com.example.demo.repositories.StockRepository;
-import com.example.demo.services.IProduitService;
-import com.example.demo.services.IStockService;
-import com.example.demo.services.StockServiceImpl;
+import tn.esprit.rh.achat.entities.*;
+import tn.esprit.rh.achat.repositories.*;
+import tn.esprit.rh.achat.services.*;
 
-
-
-import org.mockito.junit.MockitoJUnitRunner;
-
-@RunWith(MockitoJUnitRunner.Silent.class)
 @ExtendWith(MockitoExtension.class)
-@DataJpaTest 
-
-@SpringBootTest
 public class FactureTests {
-	@Mock
+	
+		@Mock
 	StockRepository StockRepo;
 	@InjectMocks
 	StockServiceImpl ss;
@@ -68,4 +46,6 @@ public class FactureTests {
 //		assertNotEquals(st.getIdStock(), null);
         //System.out.print(ss.addStock(st).getIdStock());
     }
+	
+	
 }
