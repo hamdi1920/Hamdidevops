254bf4b913ec4b7b74509ba5b1973663fe8d7c14
hamdi1920 <47883340+hamdi1920@users.noreply.github.com>
2022-11-16 23:36:31 +0100

    Update FactureTests.java

--- src/test/java/tn/esprit/rh/achat/FactureTests.java
+++ src/test/java/tn/esprit/rh/achat/FactureTests.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
+import org.junit.runner.RunWith;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
